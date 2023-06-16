addfce500bfd6f44efe7a6109d9a7f0d5584d46c
Prateek Jain <prateekjain8909@gmail.com>
2023-06-16 13:34:41 +0530

    Code refactoring

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db.init_app(app)
 
-
-@app.before_request
-def create_database():
-    db.create_all()
-    db.session.commit()
-
-
 app.route('/identify', methods=['POST'])(identify_contact)
 
 if __name__ == '__main__':
